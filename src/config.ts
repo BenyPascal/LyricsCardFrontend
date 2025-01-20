b59ed290b39df42ec77649c0ad0caed54ccd23f2
BenyPascal <149595959+BenyPascal@users.noreply.github.com>
2025-01-20 15:20:19 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-const API_URL = 'http://127.0.0.1:5000'; // Remplacez par votre URL réelle
+const API_URL = 'https://lyricscardbackend.onrender.com'; // Remplacez par votre URL réelle
 export default API_URL;
